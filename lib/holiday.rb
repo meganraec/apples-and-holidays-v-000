f61bb4e8cef18b3660d9570accaa20b58c4a398d
Megan Collier <meganraec@live.com>
2018-03-30 02:52:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, hash|
     if hash.value?("BBQ")
       hash.each do |holiday, item|
-        puts holiday
+        holiday
       end
     end
   end
